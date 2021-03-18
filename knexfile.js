694b73758c05bb819708fb9dcbed4a2561e57641
Yu Chu <cyjeff@gmail.com>
2021-03-18 14:18:41 +0900

    edited knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
+
+const parse = require("pg-connection-string").parse;
+let pgconfig = null;
+if (process.env.DATABASE_URL) {
+  pgconfig = parse(process.env.DATABASE_URL);
+  pgconfig.ssl = { rejectUnauthorized: false };
+}
+
 module.exports = {
   development: {
     client: "pg",
     connection:
-      process.env.DATABASE_URL ||
+      pgconfig ||
       `postgres://${process.env.USER}:${process.env.PASSWORD}@127.0.0.1:5432/truckstop`,
     pool: {
       min: 2,
@@ -21,8 +29,7 @@ module.exports = {
   staging: {
     client: "pg",
     connection:
-      process.env.DATABASE_URL ||
-      `postgres://${process.env.USER}@127.0.0.1:5432/truck_stops`,
+      pgconfig || `postgres://${process.env.USER}@127.0.0.1:5432/truck_stops`,
     pool: {
       min: 2,
       max: 10,
@@ -36,8 +43,7 @@ module.exports = {
   production: {
     client: "pg",
     connection:
-      process.env.DATABASE_URL ||
-      `postgres://${process.env.USER}@127.0.0.1:5432/truck_stops`,
+      pgconfig || `postgres://${process.env.USER}@127.0.0.1:5432/truck_stops`,
     pool: {
       min: 2,
       max: 10,
