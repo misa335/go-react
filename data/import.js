44ed0c68507a83cf8c93adda8d300b6ad8a239c6
Bryson Bennett <BrysonCBennett@gmail.com>
2021-03-18 11:14:03 +0900

    New branch (#6)
    * read data from the json and took out the needed data
    
    * small change
    
    * got migrations working locally at least
    
    * i dont understand branching
    
    Co-authored-by: Misako Kondoh <kondoh.misako@gmail.com>

--- data/import.js
+++ data/import.js
@@ -1,35 +1,44 @@
 const fs = require("fs");
-const db = require("../server/knex.js");np
+const db = require("../server/knex.js");
 
 (async () => {
   try {
-    
     const locations = JSON.parse(fs.readFileSync("./data/locations.json"));
     for (const location of locations) {
       const id = location.Site.SiteId;
-      const amenities = JSON.stringify(location.AdditionalAmenities.map(amenity =>amenity.SiteManagementItem.Title))
+      const amenities = JSON.stringify(
+        location.AdditionalAmenities.map(
+          (amenity) => amenity.SiteManagementItem.Title
+        )
+      );
       const latitude = location.Site.Latitude;
       const longitude = location.Site.Longitude;
       const name = location.Site.SitePreferredName;
-      const fuelPrices = JSON.stringify(location.Site.FuelPrices.map(price => {
-        return {
-          type: price.FuelType,
-          price: price.CashPrice
-        }
-      }))
-      const highway = location.Site.Highway
-      const address = JSON.stringify(location.Addresses.map( address => {
-        return {
-          address1: address.Address1,
-          address2: address.Address2,
-          city: address.City,
-          state: address.State,
-          zip: address.Zip
-        }
-      }));
+      const fuelPrices = JSON.stringify(
+        location.Site.FuelPrices.map((price) => {
+          return {
+            type: price.FuelType,
+            price: price.CashPrice,
+          };
+        })
+      );
+      const highway = location.Site.Highway;
+      const address = JSON.stringify(
+        location.Addresses.map((address) => {
+          return {
+            address1: address.Address1,
+            address2: address.Address2,
+            city: address.City,
+            state: address.State,
+            zip: address.Zip,
+          };
+        })
+      );
       const exitNumber = location.Site.ExitNumber;
-      const restaurants = JSON.stringify(location.Site.Concepts.map(concept=> concept.Concept.Name));
-      const result = {
+      const restaurants = JSON.stringify(
+        location.Site.Concepts.map((concept) => concept.Concept.Name)
+      );
+      const locationInfo = {
         id,
         amenities,
         latitude,
@@ -39,20 +48,9 @@ const db = require("../server/knex.js");np
         highway,
         address,
         exitNumber,
-        restaurants
-      }
-      const result = await db("locations").insert({
-        id,
-        amenities,
-        latitude,
-        longitude,
-        name,
-        fuelPrices,
-        highway,
-        address,
-        exitNumber,
-        restaurants
-      });
+        restaurants,
+      };
+      const result = await db("locations").insert(locationInfo);
       console.log(result);
     }
   } catch (err) {
